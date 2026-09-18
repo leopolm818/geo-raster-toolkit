22d193e5281e66ed10a792aa09a99111d4a81fff
leopolm818 <leopolm818@gmail.com>
2026-09-18 14:20:48 +0530

    Expose spectral indices in package API

--- geo_raster_toolkit/__init__.py
+++ geo_raster_toolkit/__init__.py
@@ -5,4 +5,11 @@ An open-source Python toolkit for geospatial raster analysis
 and remote sensing workflows.
 """
 
+from .indices import ndvi, ndwi
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "ndvi",
+    "ndwi",
+]
